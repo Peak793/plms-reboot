4566219cf72c1acea36a8faf3a878bb5e7f95ca3
CHANWANIT NUCHYOO <63010224@kmitl.ac.th>
2023-09-08 17:09:12 +0700

    install mui lib

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import RootLayout from '@/layouts/RootLayout';
-import MyGroups from './pages/Mygroups';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [
-      {
-        path: "mygroups",
-        element: <MyGroups />
-      },
-    ]
   },
 ]);
 
